1a3b2cd5ce8f015ffe90d356983de5305ad1b58f
Frederick Butterfly <keitumetsemogotsif@gmail.com>
2024-04-04 18:13:48 +0200

    User Progress

--- db/queries.ts
+++ db/queries.ts
@@ -1,8 +1,36 @@
-import { cache } from 'react'
-import db from "./drizzle"
+import { cache } from "react";
+import db from "./drizzle";
+import { auth } from "@clerk/nextjs";
+import { courses, userProgress } from "./schema";
+import { eq } from "drizzle-orm";
+
+export const getUserProgress = cache(async () => {
+  const { userId } = await auth();
+
+  if (!userId) {
+    return null;
+  }
+
+  const data = await db.query.userProgress.findFirst({
+    where: eq(userProgress.userId, userId),
+    with: {
+      activeCourse: true,
+    },
+  });
+
+  return data;
+});
 
 export const getCourses = cache(async () => {
-    const data = await db.query.courses.findMany()
+  const data = await db.query.courses.findMany();
 
-    return data
-})
+  return data;
+});
+
+export const getCoursesById = cache(async (courseId: number) => {
+  const data = await db.query.courses.findFirst({
+    where: eq(courses.id, courseId),
+  });
+
+  return data;
+});
